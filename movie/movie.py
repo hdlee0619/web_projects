38e5e8e1018029097ffc31edb00e32a09c6c0c74
hdlee0619 <dlgusehd1211@naver.com>
2023-01-12 17:49:54 +0900

    movie 삭제 기능 추가

--- movie/movie.py
+++ movie/movie.py
@@ -36,7 +36,9 @@ def movie_post():
     url_receive = request.form['url_give']
     star_receive = request.form['star_give']
     comment_receive = request.form['comment_give']
-
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    count = len(movie_list) + 1
+    
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
@@ -52,17 +54,23 @@ def movie_post():
         'image': image,
         'desc': desc,
         'star': star_receive,
-        'comment': comment_receive
+        'comment': comment_receive,
+        'card_num': count
     }
     db.movies.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
 
-
 @app.route("/movie", methods=["GET"])
 def movie_get():
     movie_list = list(db.movies.find({}, {'_id': False}))
     return jsonify({'movies': movie_list})
+
+@app.route('/movie/delete', methods=["POST"])
+def movie_delete():
+    num_receive = request.form['card_num_give']
+    db.movies.delete_one({'card_num' : int(num_receive)})
+    return jsonify({'msg': '삭제 완료!'})
 
 
 if __name__ == '__main__':
